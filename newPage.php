6ff3c13d6a41d072e5145cbfc391a473406e1498
pranjaldronzer <40349163+pranjaldronzer@users.noreply.github.com>
2018-06-19 11:40:25 +0530

    Update newPage.php

--- newPage.php
+++ newPage.php
@@ -9,6 +9,10 @@ class handleQuery
 	function handleQuery()
 	{
 		$query=$_SERVER['QUERY_STRING'];
+		
+		$myfile = fopen("sample.html", "r") or die("Unable to open file!");
+		echo fread($myfile,filesize("sample.html"));
+		fclose($myfile);
 		echo $query;
 	}
 }
