d95773b869bf2604833c2dac652032bd8f5c05c9
pranjaldronzer <40349163+pranjaldronzer@users.noreply.github.com>
2018-06-19 11:19:00 +0530

    Add files via upload

--- newPage.php
+++ newPage.php
@@ -12,7 +12,9 @@ class handleQuery
 		echo $query;
 
 		echo "<br>"
-
+		$myfile = fopen("sample.html", "r") or die("Unable to open file!");
+		echo fread($myfile,filesize("sample.html"));
+		fclose($myfile);
 	}
 }
 
