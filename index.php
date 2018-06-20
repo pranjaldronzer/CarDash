d2979e7e7722997be1582b516704b2f72fe7c68e
pranjaldronzer <40349163+pranjaldronzer@users.noreply.github.com>
2018-06-20 18:32:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 include 'newPage.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
+$query=$_SERVER['QUERY_STRING'];
 $requestBody = file_get_contents('php://input');
 
 filter($method,$requestBody);
@@ -11,36 +12,39 @@ function filter($request, $parameter)
 {
 	if($request == 'POST')
 	{
-	
-	$json = json_decode($parameter);
-
-	$text = $json->queryResult->queryText;
-
-	switch ($text) 
+		if ($query== "speech") 
 		{
-		case 'hi':
-			$speech = "Hi, Nice to meet you";
-			break;
-
-		case 'bye':
-			$speech = "Bye, good night";
-			break;
-
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			break;
+			
+			$json = json_decode($parameter);
 		
-		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
-			break;
-		}
-
-
-		$response = new \stdClass();
-		$response->speech = $speech;
-		$response->displayText = $speech;
-		$response->source = "webhook";
-		send(json_encode($response));
+			$text = $json->queryResult->queryText;
+		
+			switch ($text) 
+				{
+				case 'hi':
+					$speech = "Hi, Nice to meet you";
+					break;
+		
+				case 'bye':
+					$speech = "Bye, good night";
+					break;
+		
+				case 'anything':
+					$speech = "Yes, you can type anything here.";
+					break;
+				
+				default:
+					$speech = "Sorry, I didnt get that. Please ask me something else.";
+					break;
+				}
+		
+		
+				$response = new \stdClass();
+				$response->speech = $speech;
+				$response->displayText = $speech;
+				$response->source = "webhook";
+				send(json_encode($response));
+			}
 	}
 	
 	if ($request =='GET') 
